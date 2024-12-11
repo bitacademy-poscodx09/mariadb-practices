bc5b1e2f40638796ad59800846e19dffa86c084a
포스코DX 청년 IT 전문가 9기 <bitacademy.poscodx@gmail.com>
2024-12-11 17:39:29 +0900

    Update practice04.sql

--- sql-practices/practice04.sql
+++ sql-practices/practice04.sql
@@ -1,6 +1,8 @@
 -- 서브쿼리(SUBQUERY) SQL 문제입니다.
 
-SELECT 
+-- 문제1.
+-- 현재 전체 사원의 평균 급여보다 많은 급여를 받는 사원은 몇 명이나 있습니까?
+    SELECT 
     COUNT(*)
 FROM
     salaries
@@ -12,7 +14,9 @@ WHERE
             salaries
         WHERE
             to_date = '9999-01-01')
--- 단 조회결과는 급여의 내림차순으로 정렬되어 나타나야 합니다. 
+
+-- 문제2. 
+-- 현재, 각 부서별로 최고의 급여를 받는 사원의 사번, 이름, 부서 급여을 조회하세요. 단 조회결과는 급여의 내림차순으로 정렬합니다.
 SELECT 
     a.emp_no,
     CONCAT(a.first_name, ' ', a.last_name),
@@ -126,7 +130,7 @@ WHERE
 ORDER BY c.salary DESC;
 
 -- 문제6.
--- 평균 급여가 가장 높은 부서는?
+-- 현재, 평균 급여가 가장 높은 부서의 이름 그리고 평균급여를 출력하세요.
 -- 부서이름, 평균급여 
 SELECT 
     d.dept_name, ROUND(AVG(b.salary)) AS avg_salary
@@ -157,8 +161,7 @@ HAVING avg_salary = (SELECT
         GROUP BY c.dept_no) a);
         
 -- 문제7.
--- 평균 급여가 가장 높은 직책?
--- 직책, 평균급여 
+-- 현재, 평균 급여가 가장 높은 직책의 타이틀 그리고 평균급여를 출력하세요.
 SELECT 
     c.title, ROUND(AVG(b.salary)) AS avg_salary
 FROM
